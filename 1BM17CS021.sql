379741ef017cffe786dbfd497f6ea32a55458742
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2020-05-03 08:03:37 +0530

    Update 1BM17CS021.sql

--- 1BM17CS021.sql
+++ 1BM17CS021.sql
@@ -34,6 +34,29 @@ values("A04","KA095477");
 insert into OWNS(driver_id,reg_num)
 values("A05","KA041702");
 select *from CAR;
+create table ACCIDENT(
+report_num int,
+accident_date date,
+location varchar(50),
+primary key(report_num));
+insert into ACCIDENT values(11,"2003-01-01","Mysore road");
+insert into ACCIDENT values(12,"2004-02-02","South end circle"),(13,"2003-01-21","Bull temple road"),(14,"2008-02-17","Mysore road"),(15,"2005-03-04","Kanakpura road");
+select * from ACCIDENT;
+create table PARTICIPATED(
+driver_id varchar(10),
+reg_num varchar(10),
+report_num int,
+damage_amount int,
+primary key (driver_id,reg_num,report_num),
+foreign key (driver_id) references PERSON(driver_id),
+foreign key (reg_num) references CAR(reg_num),
+foreign key (report_num) references ACCIDENT(report_num));
+insert into PARTICIPATED values("A01","KA052250",11,10000),("A02","KA053408",12,50000),("A03","KA095477",13,25000),("A04","KA031181",14,3000),("A05","KA041702",15,5000);
+select * from PARTICIPATED;
+update PARTICIPATED set damage_amount=25000 where reg_num="KA053408" AND report_num=12;
+insert into ACCIDENT values(16,"2019-01-03","Hanumanth nagar");
+select count(distinct driver_id) CNT from PARTICIPATED,ACCIDENT where PARTICIPATED.report_num=ACCIDENT.report_num AND accident_date like '2008%';
+select count(distinct model) ACC_MOD from PARTICIPATED,CAR where PARTICIPATED.reg_num=CAR.reg_num AND CAR.model="Toyota";
 
 
 
